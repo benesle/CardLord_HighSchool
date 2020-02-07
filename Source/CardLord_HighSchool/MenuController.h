bf364689b604b0581971a2886418e30ec88b3587
Benedicte Slettum <34278802+benesle@users.noreply.github.com>
2020-02-07 16:04:26 +0100

    Attempt on MenuWidget

---
+++ Source/CardLord_HighSchool/MenuController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MenuController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDLORD_HIGHSCHOOL_API AMenuController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+
+	AMenuController();
+	virtual void BeginPlay() override;
+
+	
+};
