8f762485c1d9e13af4fe3f95caeba54b940d266d
Benedicte Slettum <benesl@live.no>
2020-04-15 04:50:35 +0200

    Combat with two enemis

--- Source/CardLord_HighSchool/CardLordGameInstance.cpp
+++ Source/CardLord_HighSchool/CardLordGameInstance.cpp
@@ -23,7 +23,7 @@ void UCardLordGameInstance::Init()
 	
 	if (characters == NULL)
 	{
-		UE_LOG(LogTemp, Error, TEXT("Characters data table not found!:("));
+		UE_LOG(LogTemp, Error, TEXT("Characters 'P1' data table not found!:("));
 		//return;
 	}
 
@@ -35,6 +35,30 @@ void UCardLordGameInstance::Init()
 		UE_LOG(LogTemp, Error, TEXT("Character ID 'P1' not found!!!"));
 		//return;
 	}
+
+	//Add Character to the group
+	this->GroupMembers.Add(UGameCharacter::CreateGameCharacter(row, this));
+
+	///////////////////////////////////////////////
+		//Find the characters Data
+	//
+	/*UDataTable* characters =*/ Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), NULL, TEXT("DataTable'/Game/DataTable/Character.Character'")));
+
+	if (characters == NULL)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Characters 'P2' data table not found!:("));
+		//return;
+	}
+
+	//Find characters
+	/*FCharacterData* row = */characters->FindRow<FCharacterData>(TEXT("P2"), TEXT("LookupCharcterClass"));
+
+	if (row == NULL)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Character ID 'P2' not found!!!"));
+		//return;
+	}
+	//////////////////////////////////////////
 
 	//Add Character to the group
 	this->GroupMembers.Add(UGameCharacter::CreateGameCharacter(row, this));
